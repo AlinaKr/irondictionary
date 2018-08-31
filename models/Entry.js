c0fae678c236703bd0d7104009a4d6d10a57ff6e
Maxence Bouret <maxence.bouret@gmail.com>
2018-08-31 14:46:34 +0200

    Home and dictionary pages created

--- models/Entry.js
+++ models/Entry.js
@@ -2,8 +2,18 @@ const mongoose = require('mongoose');
 const Schema   = mongoose.Schema;
 
 const entrySchema = new Schema({
-  originalWord: String,
-  convertedWord: String
+  originalWord: { 
+    type: String, 
+    required: true,
+    trim: true,
+    minlength: 1
+  },
+  convertedWord: { 
+    type: String, 
+    required: true,
+    trim: true,
+    minlength: 1
+  },
 });
 
 const Entry = mongoose.model('Entry', entrySchema);
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,48 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const router  = express.Router();
+const Entry = require('../models/Entry');
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
+});
+
+router.get('/dictionary', (req, res, next) => {
+
+  Entry.find()
+  .then(entries => {
+    res.render('dictionary', {
+      entries, 
+      error: req.query.error
+    });
+  })
+  .catch(err => next(err))
+});
+
+router.post('/entries', (req, res, next) => {
+  let { originalWord, convertedWord } = req.body
+  Entry.create({ originalWord, convertedWord })
+  .then(entry => {
+    res.redirect('/dictionary')
+  })
+  .catch(err => {
+    res.redirect('/dictionary?error=original word and converted word need to be defined')
+    // next(err)
+  })
+});
+
+router.get('/entries/:id/delete', (req, res, next) => {
+  if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+    res.redirect('/dictionary')
+    return;
+  }
+  
+  Entry.findByIdAndRemove(req.params.id)
+  .then(entry => {
+    res.redirect('/dictionary')
+  })
+  .catch(err => next(err))
 });
 
 module.exports = router;
